25c7c09e6cfcaf703de87d00c9bb782f48ff7ecd
VayFull <40316778+VayFull@users.noreply.github.com>
2019-04-22 21:39:33 +0300

    CurrentRelease

---
+++ UnityFiles/PreparingForAlgem/Assets/Scripts/CameraMoving/Rotater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public float speed = 0.1f;
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+}
---
+++ UnityFiles/PreparingForAlgem/Assets/Scripts/Ui/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void DocumentBtn()
+    {
+        Application.OpenURL("https://trello.com/b/Edn9l37I/%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%BD%D0%B0%D1%8F-%D0%B4%D0%B5%D1%8F%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C");
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void StartGameBtn()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
